b7c384a4a0cb7d4be7e7064991f3449913dfa42b
bibi <bibi6666667@kakao.com>
2021-04-01 18:13:03 +0900

    Feat : 테스트를 위한 초기 Question 데이터 추가
    테스트 편의를 위해 import.sql에 초기 Question데이터를 추가했습니다.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,5 @@
 INSERT INTO USER (USER_ID, PASSWORD, NAME, EMAIL) VALUES ('bibi', '67', 'test1', 'bibi6666667@kakao.com');
 INSERT INTO USER (USER_ID, PASSWORD, NAME, EMAIL) VALUES ('noname', '67', 'test2', 'non_named@naver.com');
+
+INSERT INTO QUESTION (id, writer_id, title, contents, date_time) VALUES (1, 1, 'test1', 'test1 contents', CURRENT_TIMESTAMP());
+INSERT INTO QUESTION (id, writer_id, title, contents, date_time) VALUES (2, 2, 'test2', 'test2 contents', CURRENT_TIMESTAMP());
